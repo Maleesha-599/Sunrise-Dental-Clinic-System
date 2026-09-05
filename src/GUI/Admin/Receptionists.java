20d91faa702b2d07b37db72f686fa3bb5b5e0ed2
Maleesha-599 <153214985+Maleesha-599@users.noreply.github.com>
2026-09-05 17:38:05 +0530

    Finalise Sunrise Dental Clinic Management System

--- src/GUI/Admin/Receptionists.java
+++ src/GUI/Admin/Receptionists.java
@@ -8,9 +8,14 @@ import Model.MySQL;
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.regex.Pattern;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
+import javax.swing.RowFilter;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -23,8 +28,16 @@ public class Receptionists extends javax.swing.JInternalFrame {
 
     java.awt.Frame Parent;
 
+    TableRowSorter<DefaultTableModel> sorter;
+
+    private javax.swing.JPopupMenu popupMenu;
+    private javax.swing.JMenuItem deleteItem;
+
     public Receptionists(java.awt.Frame parent) {
         initComponents();
+
+        sorter = new TableRowSorter<>((DefaultTableModel) jTable1.getModel());
+        jTable1.setRowSorter(sorter);
 
         setClosable(false);
         setIconifiable(false);
@@ -38,6 +51,17 @@ public class Receptionists extends javax.swing.JInternalFrame {
         Loadstatus();
 
         Parent = parent;
+
+        jButton2.setEnabled(false);
+
+        jTextField1.grabFocus();
+
+        jPopupMenu1 = new JPopupMenu();
+        deleteItem = new JMenuItem("Delete User");
+
+        jPopupMenu1.add(deleteItem);
+
+        deleteItem.addActionListener(e -> deleteSelectedRow());
 
     }
 
@@ -76,8 +100,11 @@ public class Receptionists extends javax.swing.JInternalFrame {
                 statusMap.put(resultSet.getString("status"), resultSet.getString("status_id"));
             }
 
-            DefaultComboBoxModel model = new DefaultComboBoxModel(vector);
-            jComboBox2.setModel(model);
+            DefaultComboBoxModel model1 = new DefaultComboBoxModel(vector);
+            jComboBox2.setModel(model1);
+
+            DefaultComboBoxModel model2 = new DefaultComboBoxModel(vector);
+            jComboBox4.setModel(model2);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -103,7 +130,6 @@ public class Receptionists extends javax.swing.JInternalFrame {
                 vector.add(resultSet.getString("username"));
                 vector.add(resultSet.getString("Password"));
                 vector.add(resultSet.getString("contact_no"));
-                vector.add(resultSet.getString("role.role"));
                 vector.add(resultSet.getString("status.status"));
 
                 model.addRow(vector);
@@ -115,10 +141,67 @@ public class Receptionists extends javax.swing.JInternalFrame {
 
     }
 
+    private void deleteSelectedRow() {
+
+        int row = jTable1.getSelectedRow();
+
+        if (row == -1) {
+
+            JOptionPane.showMessageDialog(this, "Please select a user first", "Warning", JOptionPane.WARNING_MESSAGE);
+
+            return;
+        }
+
+        int confirm = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete this user?", "Confirm Delete", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+
+        if (confirm == JOptionPane.YES_OPTION) {
+
+            try {
+
+                String contact_no = String.valueOf(jTable1.getValueAt(row, 3));
+
+                MySQL.executeIUD("DELETE FROM system_user WHERE contact_no = '" + contact_no + "'");
+
+                JOptionPane.showMessageDialog(this, "User deleted successfully");
+
+                LoadReceptionists();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+        } else if (confirm == JOptionPane.NO_OPTION) {
+
+            jTextField1.grabFocus();
+
+        }
+    }
+
+    private void applyFilters() {
+
+        String search = jTextField7.getText().trim();
+
+        String status = String.valueOf(jComboBox4.getSelectedItem());
+
+        java.util.List<RowFilter<Object, Object>> filters = new java.util.ArrayList<>();
+
+        if (!search.isEmpty()) {
+            filters.add(RowFilter.regexFilter("(?i)" + Pattern.quote(search), 0, 3));
+        }
+
+        if (!status.equals("Select")) {
+            filters.add(RowFilter.regexFilter("^" + Pattern.quote(status) + "$", 4));
+        }
+
+        sorter.setRowFilter(RowFilter.andFilter(filters));
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jPanel1 = new javax.swing.JPanel();
         jLabel11 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
@@ -142,10 +225,15 @@ public class Receptionists extends javax.swing.JInternalFrame {
         jTable1 = new javax.swing.JTable();
         jTextField7 = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
         jComboBox4 = new javax.swing.JComboBox<>();
-        jComboBox5 = new javax.swing.JComboBox<>();
+
+        jMenuItem1.setText("Delete");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
 
         jLabel11.setFont(new java.awt.Font("Segoe UI Black", 1, 24)); // NOI18N
         jLabel11.setForeground(new java.awt.Color(0, 0, 102));
@@ -190,7 +278,12 @@ public class Receptionists extends javax.swing.JInternalFrame {
         });
 
         jButton4.setFont(new java.awt.Font("Segoe UI Black", 1, 18)); // NOI18N
-        jButton4.setText("Delete");
+        jButton4.setText("Cancel");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -271,11 +364,11 @@ public class Receptionists extends javax.swing.JInternalFrame {
 
             },
             new String [] {
-                "Name", "Username", "Password", "Contact Number", "Role", "Status"
+                "Name", "Username", "Password", "Contact Number", "Status"
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false
+                false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -286,6 +379,12 @@ public class Receptionists extends javax.swing.JInternalFrame {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jTable1MouseClicked(evt);
             }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jTable1MousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jTable1MouseReleased(evt);
+            }
         });
         jScrollPane1.setViewportView(jTable1);
         if (jTable1.getColumnModel().getColumnCount() > 0) {
@@ -294,18 +393,29 @@ public class Receptionists extends javax.swing.JInternalFrame {
             jTable1.getColumnModel().getColumn(2).setResizable(false);
             jTable1.getColumnModel().getColumn(3).setResizable(false);
             jTable1.getColumnModel().getColumn(4).setResizable(false);
-            jTable1.getColumnModel().getColumn(5).setResizable(false);
         }
 
-        jLabel9.setText("Name");
+        jTextField7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField7ActionPerformed(evt);
+            }
+        });
+        jTextField7.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jTextField7KeyReleased(evt);
+            }
+        });
 
-        jLabel10.setText("Role");
+        jLabel9.setText("Search");
 
         jLabel12.setText("Status");
 
         jComboBox4.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
-        jComboBox5.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jComboBox4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox4ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -318,14 +428,10 @@ public class Receptionists extends javax.swing.JInternalFrame {
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(6, 6, 6)
-                        .addComponent(jTextField7, javax.swing.GroupLayout.DEFAULT_SIZE, 208, Short.MAX_VALUE)
-                        .addGap(16, 16, 16)
-                        .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 63, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jComboBox5, 0, 192, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField7, javax.swing.GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
+                        .addGap(283, 283, 283)
                         .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(16, 16, 16)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jComboBox4, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(22, 22, 22)))
                 .addContainerGap())
@@ -338,9 +444,7 @@ public class Receptionists extends javax.swing.JInternalFrame {
                     .addComponent(jTextField7)
                     .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel12, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jComboBox4)
-                    .addComponent(jComboBox5))
+                    .addComponent(jComboBox4))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 722, Short.MAX_VALUE)
                 .addContainerGap())
@@ -432,6 +536,7 @@ public class Receptionists extends javax.swing.JInternalFrame {
             }
 
         } catch (Exception e) {
+             e.printStackTrace();
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -439,15 +544,22 @@ public class Receptionists extends javax.swing.JInternalFrame {
 
         if (evt.getClickCount() == 2) {
 
-            int row = jTable1.getSelectedRow();
+            int viewRow = jTable1.getSelectedRow();
 
-            jTextField1.setText(String.valueOf(jTable1.getValueAt(row, 0)));
-            jTextField2.setText(String.valueOf(jTable1.getValueAt(row, 1)));
-            jTextField3.setText(String.valueOf(jTable1.getValueAt(row, 2)));
-            jTextField5.setText(String.valueOf(jTable1.getValueAt(row, 3)));
+            if (viewRow == -1) {
+                return;
+            }
+            
+             int row = jTable1.convertRowIndexToModel(viewRow);
 
-            jComboBox1.setSelectedItem(String.valueOf(jTable1.getValueAt(row, 4)));
-            jComboBox2.setSelectedItem(String.valueOf(jTable1.getValueAt(row, 5)));
+            jTextField1.setText(String.valueOf(jTable1.getModel().getValueAt(row, 0)));
+            jTextField2.setText(String.valueOf(jTable1.getModel().getValueAt(row, 1)));
+            jTextField3.setText(String.valueOf(jTable1.getModel().getValueAt(row, 2)));
+            jTextField5.setText(String.valueOf(jTable1.getModel().getValueAt(row, 3)));
+
+            jComboBox1.setSelectedItem("Receptionist");
+
+            jComboBox2.setSelectedItem(String.valueOf(jTable1.getModel().getValueAt(row, 4)));
 
             try {
 
@@ -480,11 +592,11 @@ public class Receptionists extends javax.swing.JInternalFrame {
             String roleId = roleMap.get(role);
             String statusId = statusMap.get(status);
 
-            // Update USER
+            
             MySQL.executeIUD("UPDATE system_user "
                     + "SET name='" + name + "', username='" + username + "', "
                     + "Password='" + password + "', contact_no='" + contactNumber + "', "
-                    + "Role_role_id='" + roleId + "' Status_status_id='" + statusId + "' "
+                    + "Role_role_id='" + roleId + "', Status_status_id='" + statusId + "' "
                     + "WHERE contact_no='" + contactNumber + "'");
 
             JOptionPane.showMessageDialog(this, "User Updated Successfully");
@@ -498,13 +610,77 @@ public class Receptionists extends javax.swing.JInternalFrame {
 
             jTextField2.setEditable(true);
             jTextField3.setEditable(true);
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+
+        reset();
+
+        jButton1.setEnabled(true);
+
+        jButton2.setEnabled(false);
+
+        jTextField2.setEditable(true);
+        jTextField3.setEditable(true);
+
+        jTable1.clearSelection();
+
+        LoadReceptionists();
+
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jTable1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MousePressed
+
+        if (evt.isPopupTrigger()) {
+
+            int row = jTable1.rowAtPoint(evt.getPoint());
+
+            if (row >= 0) {
+                jTable1.setRowSelectionInterval(row, row);
+
+                jPopupMenu1.show(jTable1, evt.getX(), evt.getY());
+            }
+        }
+    }//GEN-LAST:event_jTable1MousePressed
+
+    private void jTable1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseReleased
+
+        if (evt.isPopupTrigger()) {
+
+            int row = jTable1.rowAtPoint(evt.getPoint());
+
+            if (row >= 0) {
+                jTable1.setRowSelectionInterval(row, row);
+
+                jPopupMenu1.show(jTable1, evt.getX(), evt.getY());
+            }
+        }
+    }//GEN-LAST:event_jTable1MouseReleased
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+
+
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jTextField7KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField7KeyReleased
+
+        applyFilters();
+    }//GEN-LAST:event_jTextField7KeyReleased
+
+    private void jComboBox4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox4ActionPerformed
+
+        applyFilters();
+    }//GEN-LAST:event_jComboBox4ActionPerformed
+
+    private void jTextField7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField7ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField7ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -514,9 +690,7 @@ public class Receptionists extends javax.swing.JInternalFrame {
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JComboBox<String> jComboBox4;
-    private javax.swing.JComboBox<String> jComboBox5;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
@@ -525,9 +699,11 @@ public class Receptionists extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
+    private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPopupMenu jPopupMenu1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
@@ -545,6 +721,12 @@ public class Receptionists extends javax.swing.JInternalFrame {
 
         jComboBox1.setSelectedIndex(0);
         jComboBox2.setSelectedIndex(0);
+
+        jTextField7.setText("");
+        jComboBox4.setSelectedIndex(0);
+        sorter.setRowFilter(null);
+        jTable1.clearSelection();
+        LoadReceptionists();
 
     }
 }
